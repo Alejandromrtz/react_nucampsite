6ada2fed010ed75e1675764d14ab681dc1f57627
Alejandro Martinez <goatmix1@gmail.com>
2023-09-21 18:55:01 -0400

    added components like footer & header along with about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+
 
 function App() {
   return (
-      <div className="App">
-          <Header />
-          <CampsitesList />
-          <Footer />
-      </div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/"  element={<HomePage />} />
+        <Route path="contact"  element={<ContactPage />} />
+        <Route path="directory"  element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
