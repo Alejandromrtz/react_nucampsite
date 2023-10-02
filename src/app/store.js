70fbd87905db47149e220a1926aa2c478ce526f7
Alejandro Martinez <goatmix1@gmail.com>
2023-10-01 22:45:50 -0400

    added states

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {campsitesReducer} from '../features/campsites/campsitesSlice';
+import  {campsitesReducer} from '../features/campsites/campsitesSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer
   },
 });
+
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../features/campsites/campsitesSlice";
@@ -6,8 +7,9 @@ import CommentsList from '../features/comments/CommentsList';
 import SubHeader from '../components/SubHeader';
 
 const CampsiteDetailPage = () => {
-    const { campsiteId} = useParams();
-    const campsite = selectCampsiteById(campsiteId);
+    const { campsiteId } = useParams();
+    const campsite = useSelector(selectCampsiteById(campsiteId));
+    console.log('campsite', campsite);
 
     return(
         <Container>
